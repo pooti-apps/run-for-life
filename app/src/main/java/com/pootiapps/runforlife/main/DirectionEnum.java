e55c8aac68bff38dc65fb6ec5f50515539a024c3
karanjeetsingh <karanjeetsingh@live.in>
2015-11-07 13:41:55 -0600

    Checkin

---
+++ app/src/main/java/com/pootiapps/runforlife/main/DirectionEnum.java
@@ -0,0 +1,8 @@
+package com.pootiapps.runforlife.main;
+
+/**
+ * Created by ksingh on 11/7/2015.
+ */
+public enum DirectionEnum {
+    NE,SE,SW,NW
+}
---
+++ app/src/main/java/com/pootiapps/runforlife/main/RunForLife.java
@@ -0,0 +1,10 @@
+package com.pootiapps.runforlife.main;
+
+/**
+ * Created by ksingh on 11/7/2015.
+ */
+
+
+public class RunForLife {
+
+}
